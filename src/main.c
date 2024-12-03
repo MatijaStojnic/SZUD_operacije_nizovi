a38623f9bc8adc8e712377cfed66451aa748aaa3
MatijaStojnic <121751093+MatijaStojnic@users.noreply.github.com>
2024-12-03 18:38:49 +0100

    Pocetna verzija projekta sa osnovnim main.c fajlom

---
+++ src/main.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+int main()
+{
+    int niz[50],n;
+    printf("=================================\n");
+    printf("Pocetna verzija projekta bez ikakvih funkcija.\n");
+    printf("=================================\n");
+    return 0;
+}
