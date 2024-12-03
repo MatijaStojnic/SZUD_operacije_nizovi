dfa1d06420149f8d40e4bb8b462a5c6617e78d67
MatijaStojnic <121751093+MatijaStojnic@users.noreply.github.com>
2024-12-03 20:21:40 +0100

    Izmena početne poruke u main.c nakon implementacije funkcija.

--- src/main.c
+++ src/main.c
@@ -3,9 +3,9 @@
 int main()
 {
     int niz[50],n;
-    printf("=================================\n");
-    printf("Pocetna verzija projekta bez ikakvih funkcija.\n");
-    printf("=================================\n");
+    printf("============================================\n");
+    printf("Projekat sa svim implementiranim funkcijama!\n");
+    printf("============================================\n");
     unosniza(niz,&n);
     ispisniza(niz,n);
     max_min_prosek(niz,n);
