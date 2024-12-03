11223f90f061b520800433ace11ff81b9b1d9c67
MatijaStojnic <121751093+MatijaStojnic@users.noreply.github.com>
2024-12-03 19:46:27 +0100

    Dodavanje zaglavlja funkcije za brojanje ponavljanja elementa u nizu u funkcije.h

--- src/funkcije.h
+++ src/funkcije.h
@@ -4,4 +4,5 @@ void unosniza(int niz[], int *n);
 void ispisniza(int niz[], int n);
 void max_min_prosek(int niz[],int n);
 void sortiranje(int niz[],int n);
+void brojanjePonavljanja(int niz[], int n);
 #endif
