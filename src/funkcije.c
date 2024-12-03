253b174cb9e9991f78d80e789b2661fd1fd31436
MatijaStojnic <121751093+MatijaStojnic@users.noreply.github.com>
2024-12-03 19:19:21 +0100

    Implementacija funkcija za unos i ispis niza u funkcije.c

--- src/funkcije.c
+++ src/funkcije.c
@@ -1,1 +1,21 @@
-
+#include <stdio.h>
+#include "funkcije.h"
+void unosniza(int niz[], int *n)
+{
+    printf("Unesite broj elemenata: ");
+    scanf("%d",n);
+    printf("Unesite elemente: ");
+    for(int i=0;i<*n;i++)
+    {
+        scanf("%d", &niz[i]);
+    }
+}
+void ispisniza(int niz[], int n)
+{
+    printf("\nIspis: ");
+    for(int i=0;i<n;i++)
+    {
+        printf("%d ",niz[i]);
+    }
+    printf("\n");
+}
